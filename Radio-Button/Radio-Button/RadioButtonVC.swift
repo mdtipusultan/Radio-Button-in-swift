ab475aee8b400fe3f2bc81985b736ea487f1c4fd
Tipu <>
2023-06-15 06:08:13 +0600

    update1

---
+++ Radio-Button/Radio-Button/RadioButtonVC.swift
@@ -0,0 +1,29 @@
+//
+//  RadioButtonVC.swift
+//  Radio-Button
+//
+//  Created by Tipu on 15/6/23.
+//
+
+import UIKit
+
+class RadioButtonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Radio-Button/Radio-Button/ViewController.swift
+++ Radio-Button/Radio-Button/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var checkBoxContainer: UIView!
+    
+    @IBOutlet weak var radioButtonContainer: UIView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        radioButtonContainer.isHidden = true
     }
 
-
+    @IBAction func segmentValueChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: checkBoxContainer.isHidden = false
+        case 1: checkBoxContainer.isHidden = true
+        default: break
+        }
+        radioButtonContainer.isHidden = !checkBoxContainer.isHidden
+    }
 }
 
