9ac92378b9e3830e74cf54d017129fb392c57825
Tipu <>
2023-06-17 15:59:59 +0600

    radio- button work done

--- Radio-Button/Radio-Button/RadioButtonVC.swift
+++ Radio-Button/Radio-Button/RadioButtonVC.swift
@@ -8,22 +8,58 @@
 import UIKit
 
 class RadioButtonVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var buttonOne: UIButton!
+    
+    @IBOutlet weak var buttonTwo: UIButton!
+    
+    @IBOutlet weak var buttonThree: UIButton!
+    
+    
+    
+    var selectedButton: UIButton? // To keep track of the currently selected button
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Set initial button states
+            setButtonState(buttonOne, selected: false)
+            setButtonState(buttonTwo, selected: false)
+            setButtonState(buttonThree, selected: false)
+            
+            // Add target actions to handle button tap events
+            buttonOne.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+            buttonTwo.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+            buttonThree.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        }
+        
+        @objc func buttonTapped(_ sender: UIButton) {
+            selectButton(sender)
+        }
+        
+        func selectButton(_ button: UIButton) {
+            // Deselect the currently selected button (if any)
+            selectedButton?.isSelected = false
+            setButtonState(selectedButton, selected: false)
+            
+            // Select the tapped button
+            button.isSelected = true
+            setButtonState(button, selected: true)
+            
+            // Update the selectedButton variable
+            selectedButton = button
+        }
+        
+        func setButtonState(_ button: UIButton?, selected: Bool) {
+            guard let button = button else {
+                return
+            }
+            
+            let selectedImage = UIImage(named: "check-box")
+            let unselectedImage = UIImage(named: "check-box-empty")
+            
+            button.setImage(selected ? selectedImage : unselectedImage, for: .normal)
+        }
+    
+    
 }
